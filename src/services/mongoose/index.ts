bc62af35a619853bfaa1e9f87e07657252c149d7
Augusto Moscardo <augustomsaluz@gmail.com>
2022-09-08 23:43:04 -0300

    Removed comment

--- src/services/mongoose/index.ts
+++ src/services/mongoose/index.ts
@@ -1,36 +1,28 @@
 import mongoose from "mongoose";
 
-// let cached = global.mongoose;
+let cached = global.mongoose;
 
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 async function dbConnect() {
-  // if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  // if (!cached.promise) {
-  //   cached.promise = mongoose
-  //     .connect(process.env.DATABASE_URL as string, {
-  //       useUnifiedTopology: true,
-  //     })
-  //     .then((mongoose) => {
-  //       return mongoose;
-  //     });
-  // }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.DATABASE_URL as string)
+      .then((mongoose) => {
+        return mongoose;
+      })
+      .catch((err) => console.log(err));
+  }
 
-  // cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
 
-  // console.log(`Database Connected.`);
-
-  // return cached.conn;
-
-  const conn = await mongoose
-    .connect(process.env.DATABASE_URL as string)
-    .catch((err) => console.log(err));
   console.log("Database Connected.");
 
-  return conn;
+  return cached.conn;
 }
 
 export default dbConnect;
