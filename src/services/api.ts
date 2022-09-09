3e3828bd34c970b4dae9f65da0fc9afb53565ef8
Augusto Moscardo <augustomsaluz@gmail.com>
2022-09-08 23:43:23 -0300

    Added production URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const development = process.env.NODE_ENV !== "production";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: development
+    ? "http://localhost:3000/api"
+    : "https://todo-app-roan-three.vercel.app/api",
 });
